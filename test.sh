ee9d2b0efb32fa914e9cc0d836cce12da6974506
jamy.shih <jamy.shih@prophetstor.com>
2018-10-13 23:30:03 +0800

    Setup travis for controller-tools

--- test.sh
+++ test.sh
@@ -70,40 +70,40 @@ function header_text {
 SKIP_FETCH_TOOLS=${SKIP_FETCH_TOOLS:-""}
 
 # fetch k8s API gen tools and make it available under kb_root_dir/bin.
-function fetch_kb_tools {
-  if [ -n "$SKIP_FETCH_TOOLS" ]; then
-    return 0
-  fi
+# function fetch_kb_tools {
+#   if [ -n "$SKIP_FETCH_TOOLS" ]; then
+#     return 0
+#   fi
 
-  header_text "fetching tools"
-  kb_tools_archive_name="kubebuilder-tools-$k8s_version-$goos-$goarch.tar.gz"
-  kb_tools_download_url="https://storage.googleapis.com/kubebuilder-tools/$kb_tools_archive_name"
+#   header_text "fetching tools"
+#   kb_tools_archive_name="kubebuilder-tools-$k8s_version-$goos-$goarch.tar.gz"
+#   kb_tools_download_url="https://storage.googleapis.com/kubebuilder-tools/$kb_tools_archive_name"
 
-  kb_tools_archive_path="$tmp_root/$kb_tools_archive_name"
-  if [ ! -f $kb_tools_archive_path ]; then
-    curl -sL ${kb_tools_download_url} -o "$kb_tools_archive_path"
-  fi
-  tar -zvxf "$kb_tools_archive_path" -C "$tmp_root/"
-}
+#   kb_tools_archive_path="$tmp_root/$kb_tools_archive_name"
+#   if [ ! -f $kb_tools_archive_path ]; then
+#     curl -sL ${kb_tools_download_url} -o "$kb_tools_archive_path"
+#   fi
+#   tar -zvxf "$kb_tools_archive_path" -C "$tmp_root/"
+# }
 
-function setup_envs {
-  header_text "setting up env vars"
+# function setup_envs {
+#   header_text "setting up env vars"
 
-  # Setup env vars
-  export TEST_ASSET_KUBECTL=$kb_root_dir/bin/kubectl
-  export TEST_ASSET_KUBE_APISERVER=$kb_root_dir/bin/kube-apiserver
-  export TEST_ASSET_ETCD=$kb_root_dir/bin/etcd
-}
+#   # Setup env vars
+#   export TEST_ASSET_KUBECTL=$kb_root_dir/bin/kubectl
+#   export TEST_ASSET_KUBE_APISERVER=$kb_root_dir/bin/kube-apiserver
+#   export TEST_ASSET_ETCD=$kb_root_dir/bin/etcd
+# }
 
 header_text "using tools"
 
 which gometalinter.v2
 
 # fetch the testing binaries - e.g. apiserver and etcd
-fetch_kb_tools
+# fetch_kb_tools
 
 # setup testing env
-setup_envs
+# setup_envs
 
 header_text "running go vet"
 
